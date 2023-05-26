f9044aceffc01b0487f9fd9969ddc9b794268ea1
raphaelceccato <raphaelceccato@hotmail.com>
2023-05-26 18:15:23 -0300

    removed old code

--- leaf/src/sound.cpp
+++ leaf/src/sound.cpp
@@ -17,31 +17,4 @@ namespace leaf {
         if (alBuffer)
             alDeleteBuffers(1, (ALuint*)&alBuffer);
     }
-
-
-    /*Sound::SoundFormat Sound::identifySoundFormat(const char* filename) {
-        std::ifstream file(filename, std::ios::binary);
-        if (!file)
-            return SoundFormat::Unknown;
-
-        char signature[4];
-        if (!file.read(signature, 4)) {
-            file.close();
-            return SoundFormat::Unknown;
-        }
-
-        file.close();
-
-        if (memcmp(signature, "\x52\x49\x46\x46", 4) == 0)
-            return SoundFormat::Wav;
-        else if (memcmp(signature, "\x4F\x67\x67\x53", 4) == 0)
-            return SoundFormat::Ogg;
-        else if (memcmp(signature, "\xFF\xFB", 2) == 0)
-            return SoundFormat::Mp3;
-        else if (memcmp(signature, "fLaC", 4) == 0)
-            return SoundFormat::Flac;
-        else if (memcmp(signature, "MThd", 4) == 0)
-            return SoundFormat::Midi;
-        return SoundFormat::Unknown;
-    }*/
 }
