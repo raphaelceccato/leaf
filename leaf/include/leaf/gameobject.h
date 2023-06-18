351d217acd6be7cab8059cb5db199522358ddf4f
Raphael <raphaelceccato@hotmail.com>
2023-06-18 18:54:48 -0300

    switching to header only: gameobject.h

--- leaf/include/leaf/gameobject.h
+++ leaf/include/leaf/gameobject.h
@@ -1,19 +1,31 @@
 #pragma once
-#include "export.h"
 #include "texture.h"
 #include "animation.h"
 #include "rendertarget.h"
 
 
 namespace leaf {
-	class EXPORT GameObject {
+	class GameObject {
 	public:
-		GameObject();
+		GameObject() {
+			animator = std::make_shared<Animator>(nullptr);
+			gameObjectId = ++lastGameObjectId;
+		}
+
+
 		unsigned int getGameObjectId() const { return gameObjectId; }
 		glm::ivec2 getPosition() const { return pos; }
 		void setPosition(const glm::ivec2 pos) { this->pos = pos; }
 		AnimatorPtr getAnimator() const { return animator; }
-		void drawOnTarget(RenderTargetPtr target);
+
+
+		void drawOnTarget(RenderTargetPtr target) {
+			if (auto anim = animator->getAnimation()) {
+				auto rect = animator->getCurrentFrame();
+				target->drawEx(anim->getTexture(), pos.x, pos.y, rect.w, rect.h, rect, 0);
+			}
+		}
+
 
 	protected:
 		glm::ivec2 pos;
@@ -22,6 +34,10 @@ namespace leaf {
 	private:
 		unsigned int gameObjectId;
 		static unsigned int lastGameObjectId;
+
+		void prepareStatic() {
+			unsigned int lastGameObjectId = 0;
+		}
 	};
 
 
