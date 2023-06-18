cbc1c640ac430d516ea00db06cb3ab6afa0c87ca
Raphael <raphaelceccato@hotmail.com>
2023-06-18 18:56:32 -0300

    switching to header only: soundchannel.h

--- leaf/include/leaf/soundchannel.h
+++ leaf/include/leaf/soundchannel.h
@@ -1,24 +1,64 @@
 #pragma once
 #include <memory>
-#include "export.h"
+#include <exception>
+#include <string>
+#include <AL/al.h>
 #include "sound.h"
 
 
 namespace leaf {
-	class EXPORT SoundChannel {
+	class SoundChannel {
 	public:
-		SoundChannel(unsigned int id);
-		~SoundChannel();
-		void play(SoundPtr sound);
-		void pause();
-		void resume();
-		void stop();
-		float getVolume() const;
-		void setVolume(float value);
-		float getPitch() const;
-		void setPitch(float value);
-		bool isLooping() const;
-		void setLooping(bool loop);
+		SoundChannel(unsigned int id) : alSource(0) {
+			this->id = id;
+			alGenSources(1, (ALuint*)&alSource);
+			if (!alSource) {
+				throw std::exception(("failed to create sound source: " + std::string(alGetString(alGetError()))).c_str());
+			}
+		}
+
+
+		~SoundChannel() {
+			if (alSource)
+				alDeleteSources(1, (ALuint*)&alSource);
+		}
+
+
+		void play(SoundPtr sound) {
+			alSourcei(alSource, AL_BUFFER, sound->getALBuffer());
+			alSourcePlay(alSource);
+		}
+
+
+		float getVolume() const {
+			float value;
+			alGetSourcef(alSource, AL_GAIN, &value);
+			return value;
+		}
+
+
+		float getPitch() const {
+			float value;
+			alGetSourcef(alSource, AL_PITCH, &value);
+			return value;
+		}
+
+
+		bool isLooping() const {
+			int value;
+			alGetSourcei(alSource, AL_LOOPING, &value);
+			return (bool)value;
+		}
+
+
+		void pause() { alSourcePause(alSource); }
+		void resume() { alSourcePlay(alSource); }
+		void stop() { alSourceStop(alSource); }
+		void setVolume(float value) { alSourcef(alSource, AL_GAIN, value); }
+		void setPitch(float value) { alSourcef(alSource, AL_PITCH, value); }
+		void setLooping(bool loop) { alSourcei(alSource, AL_LOOPING, loop); }
+
+
 	private:
 		unsigned int id;
 		unsigned int alSource;
