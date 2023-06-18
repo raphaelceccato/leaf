43221918633d6e0121a9e890d2e97f1c76e3479c
Raphael <raphaelceccato@hotmail.com>
2023-06-18 20:52:17 -0300

    add leaf.h

---
+++ leaf/include/leaf/leaf.h
@@ -0,0 +1,18 @@
+#ifndef __LEAF__
+#include <memory>
+#include <glad/glad.h>
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include "soundchannel.h"
+#include "shader.h"
+#include "window.h"
+#include "engine.h"
+#include "rect.h"
+#include "color.h"
+#include "texture.h"
+#include "rendertexture.h"
+#include "timer.h"
+#include "gameobject.h"
+
+#define __LEAF__
+#endif
