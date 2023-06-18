18767bc5b5a7aae0c273f755ec713f7c59c322a2
Raphael <raphaelceccato@hotmail.com>
2023-06-18 20:53:01 -0300

    remove #include "export.h", put ptr typedef before class

--- leaf/include/leaf/rendertarget.h
+++ leaf/include/leaf/rendertarget.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "export.h"
 #include "texture.h"
 #include "shader.h"
 #include "color.h"
@@ -7,12 +6,13 @@
 
 
 namespace leaf {
+	class RenderTarget;
+	typedef std::shared_ptr<RenderTarget> RenderTargetPtr;
+
 	class RenderTarget {
 	public:
 		virtual void draw(TexturePtr tex, int x, int y, int width, int height, ShaderPtr shader = nullptr) = 0;
 		virtual void drawEx(TexturePtr tex, int x, int y, int width, int height, Rect<int> subrect, float angle, ShaderPtr shader = nullptr) = 0;
 		virtual void clear(const Color& color) = 0;
 	};
-	
-	typedef std::shared_ptr<RenderTarget> RenderTargetPtr;
 }
