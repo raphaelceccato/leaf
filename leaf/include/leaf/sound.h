b6cfa5f045873d63c72b065188bd010d246a6e06
Raphael <raphaelceccato@hotmail.com>
2023-06-18 18:56:23 -0300

    switching to header only: sound.h

--- leaf/include/leaf/sound.h
+++ leaf/include/leaf/sound.h
@@ -1,13 +1,32 @@
 #pragma once
 #include <memory>
+#include <fstream>
+#include <string>
+#include <AL/al.h>
+#include <AL/alut.h>
 
 
 namespace leaf {
     class Sound {
     public:
-        Sound(const char* path);
-        ~Sound();
-        unsigned int getALBuffer() const { return alBuffer; }
+        Sound(const char* path) {
+            alBuffer = alutCreateBufferFromFile(path);
+            if (!alBuffer)
+                throw std::exception(("failed to create sound buffer: " + std::to_string(alutGetError())).c_str());
+        }
+
+
+        ~Sound() {
+            if (alBuffer)
+                alDeleteBuffers(1, (ALuint*)&alBuffer);
+        }
+
+
+        unsigned int getALBuffer() const {
+            return alBuffer;
+        }
+
+
     private:
         unsigned int alBuffer;
 
